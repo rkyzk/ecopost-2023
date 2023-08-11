a1bc54064b53769ab6449682a5cc0b62955f0c74
rkyzk <reikoyazaki@yahoo.com>
2023-08-11 10:57:19 +0000

    make update_post.html, set the url. add update&delete btns in post_detail.html

--- blog/urls.py
+++ blog/urls.py
@@ -7,5 +7,7 @@ from django.urls import path
 urlpatterns = [ 
   path('', views.PostList.as_view(), name='home'),
   path('add_story/', views.AddStory.as_view(), name='add_story'),
+  path('update/<slug:slug>/', views.UpdatePost.as_view(),
+       name='update_post'),
   path('detail/<slug:slug>/', views.PostDetail.as_view(), name='detail_page')
 ]

381461d1fe25990b4ad3a81850eebabcff1ae7ea
rkyzk <reikoyazaki@yahoo.com>
2023-08-11 04:11:56 +0000

    set up static, media & templates folders in settings.py and make Procfile

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn ecopost2023.wsgi
--- ecopost/settings.py
+++ ecopost/settings.py
@@ -18,6 +18,7 @@ if os.path.isfile('env.py'):
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 
 # Quick-start development settings - unsuitable for production
@@ -29,7 +30,10 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-rkyzk-ecopost2023-1b6dzt1ltp1.ws-us102.gitpod.io']
+ALLOWED_HOSTS = [
+    '8000-rkyzk-ecopost2023-1b6dzt1ltp1.ws-us102.gitpod.io'
+    'ecopost2023-9f46edbaba4f.herokuapp.com'
+]
 
 
 # Application definition
@@ -40,7 +44,9 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'cloudinary_storage',
     'django.contrib.staticfiles',
+    'cloudinary',
     'blog'
 ]
 
@@ -59,7 +65,7 @@ ROOT_URLCONF = 'ecopost.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -126,6 +132,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
+STOTICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = '/media'
+DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field

c49fb316a5e18e1724d9f027f472caea7f34c8db
rkyzk <reikoyazaki@yahoo.com>
2023-08-11 07:35:46 +0000

    make PostDetail view

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views import generic
+from django.views import generic, View
 from .models import Post
 
 
@@ -9,3 +9,40 @@ class PostList(generic.ListView):
     queryset = Post.objects.filter(featured_flag=True).order_by(
         "-created_on")[:3]
     template_name = "index.html"
+
+
+class PostDetail(View):
+    """
+    Displays the full content of a post and the comments on 'post detail' page.
+    Displays a comment form for users to leave comments.
+    """
+    def get(self, request, slug, *args, **kwargs):
+        """
+        renders 'post detail' page.
+        argument: self, request, slug
+        :return: render()
+        :rtype: method
+        """
+        post = get_object_or_404(Post, slug=slug)
+        comments = post.comments.order_by('created_on')
+        # If the user has liked the post, set 'liked' True
+        liked = False
+        if post.likes.filter(id=self.request.user.id).exists():
+            liked = True
+        # If the user has bookmarked the post, set 'bookmarked' True
+        bookmarked = False
+        if post.bookmark.filter(id=self.request.user.id).exists():
+            bookmarked = True
+        return render(
+            request,
+            "post_detail.html",
+            {
+                "post": post,
+                "comments": comments,
+                "liked": liked,
+                "bookmarked": bookmarked,
+                "comment_form": CommentForm(),
+            },
+        )
+
+

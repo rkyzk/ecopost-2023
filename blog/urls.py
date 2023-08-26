b9ae0c67839c32b6f052eeea2999b995f224bf68
rkyzk <reikoyazaki@yahoo.com>
2023-08-26 01:55:23 +0000

    style recent posts page. change template's name

--- blog/urls.py
+++ blog/urls.py
@@ -7,8 +7,8 @@ from django.urls import path
 urlpatterns = [
   path('', views.PostList.as_view(), name='home'),
   path('add_story/', views.AddStory.as_view(), name='add_story'),
-  path('more_stories/', views.MoreStories.as_view(), name='more_stories'),
-  path('popular_stories/', views.PopularStories.as_view(),
+  path('recent_stories/', views.RecentPosts.as_view(), name='recent_stories'),
+  path('popular_stories/', views.PopularPosts.as_view(),
        name='popular_stories'),
   path('search/', views.SearchPosts.as_view(), name='search'),
   path('my_page/<int:pk>/', views.MyPage.as_view(), name='my_page'),

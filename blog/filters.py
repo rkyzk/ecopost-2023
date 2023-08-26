64db2fdedd40f02777ec95953fff0387c9d02101
rkyzk <reikoyazaki@yahoo.com>
2023-08-26 01:46:49 +0000

    update filters.py

--- blog/filters.py
+++ blog/filters.py
@@ -1,21 +1,33 @@
 import django_filters
 from .models import Post, CATEGORY
+from django.forms.widgets import Input
+from django.db.models.functions import Trim
 from django.db.models import Q
+from datetime import datetime
 
 
 class PostFilter(django_filters.FilterSet):
-    title = django_filters.CharFilter(lookup_expr='icontains')
+    title = django_filters.CharFilter(lookup_expr='icontains',
+                                      label="title")
     author__username = django_filters.CharFilter(lookup_expr='icontains',
-                                                 label='Author')
+                                                 label='author')
     published_after = django_filters.DateFilter(method='filter_start_date',
-                                                label='published_after')
+                                                label='from',
+                                                widget=Input(
+                                                    attrs={'type': 'date'}))
     published_before = django_filters.DateFilter(method='filter_end_date',
-                                                 label='published_before')
+                                                 label='until',
+                                                 widget=Input(
+                                                    attrs={'type': 'date'}))
     num_of_likes = django_filters.NumberFilter(method='filter_likes',
-                                               label='liked more than')
-    category = django_filters.ChoiceFilter(choices=CATEGORY)
+                                               label='liked more than',
+                                               widget=Input(
+                                                    attrs={'min': int(0),
+                                                           'type': 'number'}))
+    category = django_filters.ChoiceFilter(choices=CATEGORY,
+                                           empty_label='Choose...')
     city = django_filters.CharFilter(lookup_expr='icontains')
-    search_keyword = django_filters.CharFilter(method='filter_keyward',
+    keyword = django_filters.CharFilter(method='filter_keyword',
                                                label='keyword')
 
     class Meta:
@@ -34,4 +46,5 @@ class PostFilter(django_filters.FilterSet):
         return queryset.filter(Q(published_on__gte=value))
 
     def filter_end_date(self, queryset, name, value):
-        return queryset.filter(Q(published_on__lte=value))
+        end_date = datetime.combine(value, datetime.max.time())
+        return queryset.filter(Q(published_on__lte=end_date))

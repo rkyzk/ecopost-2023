7eed3b4eb96890450e4f9a41c7756e3d5b3f480e
rkyzk <reikoyazaki@yahoo.com>
2023-08-26 02:39:17 +0000

    prepopulate the slug and add publish_posts method in PostAdmin in blog.admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,10 @@
+"""This module holds classes to customize the admin panel."""
+
 from django.contrib import admin
 from django.db import models
 from .models import Post, Comment
 from django_summernote.admin import SummernoteModelAdmin
+from datetime import datetime
 
 
 @admin.register(Post)
@@ -9,8 +12,19 @@ class PostAdmin(SummernoteModelAdmin):
     """Customizes the appearance of Post model on the admin panel."""
     list_display = ('title', 'slug', 'status', 'created_on')
     search_fields = ('title', 'content')
+    prepopulated_fields = {'slug': ('title',)}
     list_filter = ('status', 'published_on')
     summernote_fields = ('content',)
+    actions = ['publish_posts']
+
+    def publish_posts(self, request, queryset):
+        """
+        Sets the status to 'Published'
+        and stores the published date and time.
+        arguments: self, request, queryset: posts to be published
+        """
+        queryset.update(status=1)
+        queryset.update(published_on=datetime.utcnow())
 
 
 @admin.register(Comment)
